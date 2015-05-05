2465635c7be34114f3bf0873934e2befda0d34c0
TearTheSky <the.everglow.m.a.e@gmail.com>
2015-05-05 07:22:34 +0000

    add Hubot proccess starting script sample.

---
+++ benri-kun_start_sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export HUBOT_SLACK_TOKEN=tartweafsdfasdf-999999999999-fasdaJitrea9dafFJ
+export HUBOT_SLACK_TEAM=YourTeamName
+export HUBOT_SLACK_BOTNAME=benri-kun
+
+bin/hubot --adapter slack -r src
+
